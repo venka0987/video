5493baafff255d7ee0062d5fc1c04195dde7aeb9
Sylvain Filoni <sylvain.fillion15@gmail.com>
2022-09-02 11:24:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from PIL import Image
 import os
 MY_SECRET_TOKEN=os.environ.get('HF_TOKEN_SD')
 
-from diffusers import StableDiffusionPipeline
-#from diffusers import StableDiffusionImg2ImgPipeline
+#from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionImg2ImgPipeline
 
 print("hello sylvain")
 
@@ -16,15 +16,15 @@ YOUR_TOKEN=MY_SECRET_TOKEN
 
 device="cpu"
 
-pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=YOUR_TOKEN)
+pipe = StableDiffusionImg2ImgPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=YOUR_TOKEN)
 pipe.to(device)
 
 gallery = gr.Gallery(label="Generated images", show_label=False, elem_id="gallery").style(grid=[2], height="auto")
 
-def infer(prompt): 
+def infer(prompt, init_image): 
     
     #image = pipe(prompt, init_image=init_image)["sample"][0]
-    images_list = pipe([prompt] * 4)
+    images_list = pipe([prompt] * 2, init_image=init_image, strength=0.75)
     images = []
     safe_image = Image.open(r"unsafe.png")
     for i, image in enumerate(images_list["sample"]):
@@ -40,4 +40,4 @@ print("Great sylvain ! Everything is working fine !")
 title="Stable Diffusion CPU"
 description="Stable Diffusion example using CPU and HF token. <br />Warning: Slow process... ~5/10 min inference time. <b>NSFW filter enabled.</b>" 
 
-gr.Interface(fn=infer, inputs="text", outputs=gallery,title=title,description=description).launch(enable_queue=True)
+gr.Interface(fn=infer, inputs=["text","image"], outputs=gallery,title=title,description=description).launch(enable_queue=True)
